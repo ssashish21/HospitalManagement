0e17a772df6bb44d9b590bc81af20151e5f20051
Ashish Pradhan <ssashish21@gmail.com>
2025-08-01 20:58:27 +0530

    Initial commit

---
+++ src/main/java/com/ashish/hospital/hospitalManagement/HospitalManagementApplication.java
@@ -0,0 +1,15 @@
+package com.ashish.hospital.hospitalManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementApplication.class, args);
+		System.out.println("hello");
+	}
+
+
+}
