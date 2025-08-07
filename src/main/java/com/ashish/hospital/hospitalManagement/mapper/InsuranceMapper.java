a62c20137315adf63b465a0e9c8aa23972a8407c
Ashish Pradhan <ssashish21@gmail.com>
2025-08-07 22:22:23 +0530

    Added InsuranceMapper
    This class is used for converting Entity to DTO and DTO to Entity

---
+++ src/main/java/com/ashish/hospital/hospitalManagement/mapper/InsuranceMapper.java
@@ -0,0 +1,19 @@
+package com.ashish.hospital.hospitalManagement.mapper;
+
+import com.ashish.hospital.hospitalManagement.dtos.insurance.InsuranceRequest;
+import com.ashish.hospital.hospitalManagement.dtos.insurance.InsuranceResponse;
+import com.ashish.hospital.hospitalManagement.dtos.patient.PatientResponse;
+import com.ashish.hospital.hospitalManagement.entity.Insurance;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface InsuranceMapper {
+    Insurance toEntity(InsuranceRequest request);
+    InsuranceResponse toResponse(Insurance insurance);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromRequest(InsuranceRequest request, @MappingTarget Insurance insurance);
+}
